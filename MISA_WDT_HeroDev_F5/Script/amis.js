6fe92ed12097dc70cadee53cd5b10ed63228ac9c
= <33550236+trunghieu2609vn@users.noreply.github.com>
2018-09-11 17:03:48 +0700

    ver2

--- MISA_WDT_HeroDev_F5/Script/amis.js
+++ MISA_WDT_HeroDev_F5/Script/amis.js
@@ -12,3 +12,26 @@ $('.img-catalog-app').click(function () {
 $('.r-user').click(function () {
 	$('.r-user-profile').slideToggle(150);
 });
+
+var check = 2;
+$('.nav-menu').click(function() {
+	if (check%2 == 0) {
+		$('.nav-logo').toggle(1);
+		$('.search-box').css('min-width','390px');
+		$('#ip-search').css('width','80%');
+		$('.nav-panel-l-r').css('width','27.8%');
+		$('.catalog-application-catalog').css('left','24px');
+		$('.setting-dialog').css('width','28.5%');
+		$('.nav-menu').css('box-shadow','7px -2px 36px -5px #003f7f');
+	}else{
+		$('.nav-logo').toggle(1);
+		$('.search-box').css('min-width','300px');
+		$('#ip-search').css('width','74%');
+		$('.nav-panel-l-r').css('width','20.4%');
+		$('.catalog-application-catalog').css('left','206px');
+		$('.setting-dialog').css('width','22%');
+		$('.nav-menu').css('box-shadow','0 0 0 0');
+	}
+	check++;
+	console.log(check);
+});
