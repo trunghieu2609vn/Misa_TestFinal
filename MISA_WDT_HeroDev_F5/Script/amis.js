47dc11f0bc2413c0f0f9fb3d39c58bd1cb612857
= <33550236+trunghieu2609vn@users.noreply.github.com>
2018-09-11 11:02:37 +0700

    ver1

---
+++ MISA_WDT_HeroDev_F5/Script/amis.js
@@ -0,0 +1,14 @@
+$('.setting-search').click(function() {
+	$('.setting-dialog').toggle(300);
+	$('.triangle').slideUp(200);
+	$('.catalog-application-catalog').slideUp(200);
+});
+
+$('.img-catalog-app').click(function () {
+	$('.triangle').slideToggle(200);
+	$('.catalog-application-catalog').slideToggle(200);
+	$('.setting-dialog').slideUp(200);
+});
+$('.r-user').click(function () {
+	$('.r-user-profile').slideToggle(150);
+});
